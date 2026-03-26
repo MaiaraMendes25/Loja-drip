0c27faac6b4f0c93b2346636295f042b04b06251
maiara mendes da silva <maiara@Eliass-MacBook-Air.local>
2026-03-25 21:54:27 -0300

    fix build vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Importe o plugin
 
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(), 
+    react()
   ],
 });
